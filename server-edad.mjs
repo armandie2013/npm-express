ece581e628d9846936703d6618fafa0b54060476
= <=>
2025-02-17 17:28:52 -0300

    Ordenado

--- server-edad.mjs
+++ server-edad.mjs
@@ -1,18 +1,16 @@
-import express from 'express';
+import express from "express";
 
 // Creamos una instancia y definimos el puerto de escucha del servidor //
 const app = express();
 const PORT = 3000;
 
-
-// Definimos la ruta GET 
-app.get('/profile', (req, res) => {
-    const edad = req.query.edad;
-    console.log(`Edad recibida: ${edad}`);
-    res.send(`Edad del perfil: ${edad}`);
-
+// Definimos la ruta GET
+app.get("/profile", (req, res) => {
+  const edad = req.query.edad;
+  console.log(`Edad recibida: ${edad}`);
+  res.send(`Edad del perfil: ${edad}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
